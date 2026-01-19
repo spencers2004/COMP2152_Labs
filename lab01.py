d30a5d6e202bc30c3a0864445ea4fc3e0a61023d
spencers2004 <spencer.sargeant@georgebrown.ca>
2026-01-18 19:37:44 -0500

    Add files via upload

---
+++ lab01.py
@@ -0,0 +1,23 @@
+# Sample Coding Questions 01 Week 01
+#Spencer Sargeant
+
+#Question 2: Defining an array (list)
+my_array = [1, 4, 7, 9]
+
+#Question 3
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = a - ((b ** c) // d) + (a % c)
+
+#Question 4
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+#Question 5
+print("Enter your age")
+userAge = int(input())
+userAge = userAge + 22
+print(f"Now showing the shop items filtered by age: {userAge}")
